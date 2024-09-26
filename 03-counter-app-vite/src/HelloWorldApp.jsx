9cb34ea0c0f143929220509b0c7f8e6467d32abe
leummass <jesuval420@gmail.com>
2024-09-26 16:42:33 -0700

    Segundo commit, avances

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+/*
+export function App() {
+  //
+  return <h1>H</h1>;
+}
+
+export const HelloWorldApp = () => {
+  return <div> Hello World App </div>;
+}; 
+*/
+
+
+export const HelloWorldApp = () =>  <h1>Arrow Function</h1>
